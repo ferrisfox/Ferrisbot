a7da9f2e454eab1619cb7f1887d3a970cbca66c0
ferrisfox <accounts@sneakysneakysneaky.com>
2019-02-25 21:40:00 +1030

    added/fixed response to mentions

--- bot.rb
+++ bot.rb
@@ -32,7 +32,8 @@ end
 
 
 
-bot.message(content: '@Ferrisbot#1235 ') do |event|
+
+bot.message(content: '@Ferrisbot#1235') do |event|
     event << 'Hey @' + event.user.name + ' You can use !help for a list of what I can do'
 end
 
