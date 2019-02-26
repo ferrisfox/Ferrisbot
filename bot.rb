fe052cdb07e3d72753b20b5089ac697d5ab77de2
ferrisfox <accounts@sneakysneakysneaky.com>
2019-02-26 20:13:14 +1030

    moved alot of variables into #{...}

--- bot.rb
+++ bot.rb
@@ -13,7 +13,6 @@ puts "This bot's invite URL is: #{bot.invite_url}"
 BOT_ADMINS = YAML.load(File.open('Config.conf', 'r').read)['Admins']
 
 def is_admin (user)
-    #user = user.user unless user.respond_to?(:pm)  #tries to fix any cases of bad input. just give propper input insted.
     return BOT_ADMINS.include? user
 end
 
@@ -37,7 +36,7 @@ end
 
 bot.mention() do |event|
     break unless event.content.length <= 21
-    event << 'Hey ' + event.user.mention + ' You can use !help for a list of what I can do'
+    event << "Hey #{event.user.mention} You can use !help for a list of what I can do"
 end
 
 
@@ -47,16 +46,16 @@ end
 
 bot.command(:roll, description: 'Roll a dice') do |_event, sides|
     sides = 6 unless sides.to_i >= 1
-    return 'Rolled a ' + (rand(sides.to_i) + 1).to_s + '.'
+    return "Rolled a #{(rand(sides.to_i) + 1).to_s}."
 end
 
 bot.command(:coin, description: 'Flip a coin') do |_event|
-    return 'Landed on ' + ['heads', 'tails'][rand(2)] + '.'
+    return "Landed on #{['heads', 'tails'][rand(2)]}."
 end
 
 bot.command(:rps) do |event, player_choice|
     bot_int = rand(3)
-    event << 'I chose ' + ['rock', 'paper', 'scissors'][bot_int] + '!'
+    event << "I chose #{['rock', 'paper', 'scissors'][bot_int]}!"
 
     case player_choice.to_s.downcase
     when 'rock', 'r'
